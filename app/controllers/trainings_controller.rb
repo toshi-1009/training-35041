beda00393612976407cb93b5fb67e93e18abbe4d
toshi-1009 <dearstage0429@gmail.com>
2021-05-07 17:40:54 +0900

    トレーニング投稿機能、一覧表示機能の実装

--- app/controllers/trainings_controller.rb
+++ app/controllers/trainings_controller.rb
@@ -1,8 +1,7 @@
 class TrainingsController < ApplicationController
 
 def index
-  get_week
-  @training = Training.new
+  @trainings = Training.all
 end
 
 def new
@@ -10,9 +9,12 @@ def new
 end
 
 def create
-  # binding.pry
-  Training.create(training_params)
-  redirect_to action: :index
+  @trainings = Training.new(training_params)
+  if @trainings.save
+    redirect_to root_path 
+  else
+    render :new
+  end 
 end
 
 def show
@@ -34,43 +36,4 @@ private
 def training_params
   params.require(:training).permit(:date, :training_menu, :category_id, :point,:evaluation).merge(user_id: current_user.id)
 end
-
-def get_week
-
-  wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
-  
-  # Dateオブジェクトは、日付を保持しています。下記のように`.today.day`とすると、今日の日付を取得できます。
-  @todays_date = Date.today
-  # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
-
-  @week_days = []
-
-  plans = Training.where(date: @todays_date..@todays_date + 6)
-
-  7.times do |x|
-    today_plans = []
-    plans.each do |plan|
-      today_plans.push(plan.category.name, plan.training_menu, plan.point, plan.evaluation) if plan.date == @todays_date + x
-    end
-
-
-    wday_num = Date.today.wday + x
-
-    if wday_num >= 7
-
-    wday_num = wday_num -7
-
-    end
-
-
-    days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num] } 
-
-    @week_days.push(days)
-  end
-
 end
-end
-
-
-
-
